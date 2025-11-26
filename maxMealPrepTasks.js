843106906623de3d218bcabad7b02e7ce48aba95
Quincy Williams <williams.q@outlook.com>
2025-11-25 23:07:02 -0500

    Update maxMealPrepTasks.js
    Added logic to track and count non-overlapping meal prep tasks.

--- maxMealPrepTasks.js
+++ maxMealPrepTasks.js
@@ -7,13 +7,23 @@ function maxMealPrepTasks(tasks) {
         chosen: []
     }
 
+    // Create a tracker that will compare end times of the previous task to the start time
+    // of the current task
+    let previousEndTime = 0;
+
     // Loop through the arrays in the tasks array to compare the times for each task
     // against another tasks times to check for overlap
     // If times overlap, choose the corresponding task that would come first
+
     for (let i = 0; i < tasks.length; ++i) {
-        for (let j = 1; j < tasks.length; ++j) {
+        if (tasks[i][1] >= previousEndTime) {
+            possibleTasks.count += 1;
+            possibleTasks.chosen.push(tasks[i][0]);
+            previousEndTime = tasks[i][2];
         }
     }
+
+    return possibleTasks;
 }
 
 
@@ -23,3 +33,5 @@ const tasks = [
     ["Bake Rolls", 11, 13],
     ["Prep Salad", 12, 13]
 ];
+
+maxMealPrepTasks(tasks);
