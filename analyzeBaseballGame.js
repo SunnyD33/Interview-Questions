6836ce6c3663521ccdbb077c3348dcf468efccb5
Quincy Williams <williams.q@outlook.com>
2026-01-13 08:46:43 -0500

    Refactor analyzeBaseballGame for efficiency
    Updated the analyzeBaseballGame function to directly update gameStats for home and away totals during the loop, and removed unnecessary variables.

--- analyzeBaseballGame.js
+++ analyzeBaseballGame.js
@@ -1,50 +1,45 @@
 function analyzeBaseballGame(gameArray) {
+  // Object to be returned with data from gameArray
+  const gameStats = {
+    homeTotal: 0,
+    awayTotal: 0,
+    homeLedInnings: [],
+    awayLedInnings: [],
+    winner: "",
+  };
 
-    // Object to be returned with data from gameArray
-    const gameStats = {
-        homeTotal: 0,
-        awayTotal:0,
-        homeLedInnings: [],
-        awayLedInnings: [],
-        winner: "",
-    }
+  // Loop through each array in game array and  get the values for each team
+  // in each inning
+  for (let i = 0; i < gameArray.length; ++i) {
+    gameStats.homeTotal += gameArray[i][0];
+    gameStats.awayTotal += gameArray[i][1];
 
-    // Create accumulators for the total scores
-    let homeScoreTotal = 0;
-    let awayScoreTotal = 0;
-
-    // Loop through each array in game array and  get the values for each team
-    // in each inning
-    for (let i = 0; i < gameArray.length; ++i) {
-            homeScoreTotal += gameArray[i][0];
-            awayScoreTotal += gameArray[i][1];
-
-            // For each iteration, push the winning inning to each respective team
-            if(gameArray[i][0] > gameArray[i][1]) {
-                gameStats.homeLedInnings.push(i + 1);
-            } else if (gameArray[i][0] < gameArray[i][1]) {
-                gameStats.awayLedInnings.push(i + 1);
-            } else {
-                continue;
-            }
-    }
-
-    // Set total scores accumulated to the proper fields in the gameStats object
-    gameStats.homeTotal = homeScoreTotal;
-    gameStats.awayTotal = awayScoreTotal;
-
-    // Determine winner of the game based on the totals
-    if (homeScoreTotal > awayScoreTotal) {
-        gameStats.winner = "home";
-    } else if (homeScoreTotal < awayScoreTotal){
-        gameStats.winner = "away"
+    // For each iteration, push the winning inning to each respective team
+    if (gameArray[i][0] > gameArray[i][1]) {
+      gameStats.homeLedInnings.push(i + 1);
+    } else if (gameArray[i][0] < gameArray[i][1]) {
+      gameStats.awayLedInnings.push(i + 1);
     } else {
-        gameStats.winner = "tie"
+      continue;
     }
+  }
 
-    return gameStats;
- }
- 
+  // Determine winner of the game based on the totals
+  if (homeScoreTotal > awayScoreTotal) {
+    gameStats.winner = "home";
+  } else if (homeScoreTotal < awayScoreTotal) {
+    gameStats.winner = "away";
+  } else {
+    gameStats.winner = "tie";
+  }
 
- const innings = [[1,0], [2,2], [0,3], [4,1]];
- analyzeBaseballGame(innings);
+  return gameStats;
+}
+
+const innings = [
+  [1, 0],
+  [2, 2],
+  [0, 3],
+  [4, 1],
+];
+console.log(analyzeBaseballGame(innings));
